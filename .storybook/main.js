13366b45a572b2a83c10a4c832e839cad7ab1616
Pedro Silva <pedro_dith@hotmail.com>
2023-05-13 17:05:20 -0300

    add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  stories: ['../.storybook/stories/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};
