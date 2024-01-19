52152c51a5de29f5d6a8280d9157824305a2995a
Pedro Silva <pedro_dith@hotmail.com>
2024-01-19 20:33:48 -0300

    remove different folders

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'index.ts'),
+      name: 'landingpage-creator-components',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDom',
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
       src: '/src',
